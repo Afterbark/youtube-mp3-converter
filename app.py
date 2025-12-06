0ea227c04bb58da6d6e3fc966be1c588dd394add
unknown <you@example.comzaidjazar098@gmail.com>
2025-12-06 20:19:06 +0300

    trying to get preview back

--- app.py
+++ app.py
@@ -1722,3 +1722,6 @@ https://www.youtube.com/watch?v=..."></textarea>
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=False)
+    
+    
+    
